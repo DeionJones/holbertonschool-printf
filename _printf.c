76aeb3c781677d64a5c298cbd858d60cf530868d
deionjones <5553@holbertonstudents.com>
2022-10-28 08:44:29 -0700

    1

---
+++ _printf.c
@@ -0,0 +1,10 @@
+#include "main.h"
+
+/**
+ * int _printf(const char *format, ...) - prints number of characters
+ * @format: character string
+ * Return - number of characters
+ */
+int _printf(const char *format, ...)
+{
+	
