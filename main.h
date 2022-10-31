1b151a61afd95d5f0b91a40a9f94a618f11311e7
linszay <lindseyjlancaster@gmail.com>
2022-10-31 13:45:42 -0500

    modified _print_format file to make it betty compliant

--- main.h
+++ main.h
@@ -9,4 +9,7 @@ void print_c(va_list ar);
 void print_s(va_list ar);
 void print_i(va_list ar);
 void _recursion_integer(int i);
+int _print_format(const char *format, va_list args);
+int _print_spec(char format, va_list args);
+int _validate_char(char _type);
 #endif
