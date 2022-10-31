b8a41cbc11f824196a14a499a83060a114f02673
linszay <lindseyjlancaster@gmail.com>
2022-10-31 15:21:27 -0500

    added prototypes to main.h

--- main.h
+++ main.h
@@ -20,5 +20,6 @@ void print_i(va_list ar);
 void _recursion_integer(int i);
 int _print_format(const char *format, va_list args);
 int _print_spec(char format, va_list args);
+int _print_invalid_spec(char prev_format, char format, int count);
 int _validate_char(char _type);
 #endif
