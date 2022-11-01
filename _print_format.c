acd21c63ec5f976725ec83ce6e9aaa52c65c12d9
linszay <lindseyjlancaster@gmail.com>
2022-11-01 14:51:11 -0500

    added betty doc styling for print format

--- _print_format.c
+++ _print_format.c
@@ -10,109 +10,114 @@
  */
 int _print_format(const char *format, va_list args)
 {
-  int count = 0;
-  int i = 0;
+	int count = 0;
+	int i = 0;
 
-  while (format && format[i])
-  {
-    if (format[i] == '%')
+	while (format && format[i])
 	{
-      if (format[i + 1] == '\0')
-        return (-1);
+		if (format[i] == '%')
+		{
+			if (format[i + 1] == '\0')
+				return (-1);
 
-      i++;
+			i++;
 
-      while (format[i] == ' ')
-        i++;
+			while (format[i] == ' ')
+				i++;
 
-      if (format[i] == '%')
-        count += _putchar(format[i]);
+			if (format[i] == '%')
+				count += _write(format[i]);
 
-      if (_validate_char(format[i]) == 0)
-	  {
-        count = _print_invalid_spec(format[i - 1], format[i], count);
-      }
-	  else
-	  {
-        count += _print_spec(format[i], args);
-      }
-    }
-	else
-	{
-      count += _putchar(format[i]);
-    }
+			if (_validate_char(format[i]) == 0)
+			{
+				count = _print_invalid_spec(format[i - 1], format[i], count);
+			}
+			else
+			{
+				count += _print_spec(format[i], args);
+			}
+		}
+		else
+		{
+			count += _write(format[i]);
+		}
 
-    i++;
-  }
+		i++;
+	}
 
-  return (count);
+	return (count);
 }
 
 /**
- * _print_spec - Prints a valid specifier
- * @format: The specifier to print
- * @args: A list of variadic arguments
- *
- * Return: The length of the specifier
- */
+  * _print_spec - Prints a valid specifier
+  * @format: The specifier to prints
+  * @args: A list of variadic arguments
+  *
+  * Return: The length of the specifier
+  */
 int _print_spec(char format, va_list args)
 {
-  int i = 0, length = 0;
-  spc_dt _types[] = {{"c", print_c},
-                     {"s", print_s},
-                     {"d", print_i},
-                     {"i", print_i},
-                     {NULL, NULL}};
+	int i  = 0, length = 0;
+	spc_dt _types[] = {
+		{"c", _print_a_char},
+		{"s", _print_a_string},
+		{"d", _print_a_integer},
+		{"i", _print_a_integer},
+		{NULL, NULL}
+	};
 
-  while (_types[i].specifier)
-  {
-    if (*_types[i].specifier == format)
-      length = _types[i].f(args);
+	while (_types[i].specifier)
+	{
+		if (*_types[i].specifier == format)
+			length = _types[i].f(args);
 
-    i++;
-  }
+		i++;
+	}
 
-  return (length);
+	return (length);
 }
 
 /**
- * _print_invalid_spec - Prints an invalid specifier
- * @prev_format: The previous specifier of actual specifier
- * @format: The specifier to prints
- * @count: The current count before prints invalid specifiers
- *
- * Return: The current count after prints invalid specifiers
- */
+  * _print_invalid_spec - Prints an invalid specifier
+  * @prev_format: The previous specifier of actual specifier
+  * @format: The specifier to prints
+  * @count: The current count before prints invalid specifiers
+  *
+  * Return: The current count after prints invalid specifiers
+  */
 int _print_invalid_spec(char prev_format, char format, int count)
 {
-  count += _putchar('%');
+	count += _write('%');
 
-  if (prev_format == ' ')
-  {
-    count += _putchar(' ');
-    count += _putchar(format);
-  } else {
-    count += _putchar(format);
-  }
+	if (prev_format == ' ')
+	{
+		count += _write(' ');
+		count += _write(format);
+	}
+	else
+	{
+		count += _write(format);
+	}
 
-  return (count);
+	return (count);
 }
 
 /**
- * _validate_char - validate the type
- * @_type: character to be comparate
- *
- * Return: 1 if char is equal to a type
- */
+  * _validate_char - validate the type
+  * @_type: character to be comparate
+  *
+  * Return: 1 if char is equal to a type
+  */
 int _validate_char(char _type)
 {
-  char _types[] = {'c', 's', 'd', 'i', '%'};
-  int i = 0;
+	char _types[] = {'c', 's', 'd', 'i', '%'};
+	int i = 0;
 
-  while (_types[i]) {
-    if (_types[i] == _type)
-      return (1);
-    i++;
-  }
-  return (0);
+	while (_types[i])
+	{
+		if (_types[i] == _type)
+			return (1);
+		i++;
+	}
+	return (0);
 }
